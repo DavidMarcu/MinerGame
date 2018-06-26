6cdbd9ac467719e5800d6d36c716e5a10569a130
codrinalex1994 <codrinalex1994@yahoo.com>
2018-06-26 17:54:32 +0300

    generate blocks update

--- MinerGame/Assets/Scripts/GenerateBlocks.cs
+++ MinerGame/Assets/Scripts/GenerateBlocks.cs
@@ -6,20 +6,27 @@ public class GenerateBlocks : MonoBehaviour {
 
     public GameObject block;
 
-    private float xCoord = -4f;
-    private float yCoord = 0f;
-
-    private int noBlocksPerColumn = 9;
-    private int noBlocksPerRow = 14;
-
     // Use this for initialization
     void Start () {
+        float xCoord = -4f;
+        float yCoord = 0f;
+
+        int noBlocksPerColumn = 9;
+        int noBlocksPerRow = 14;
+
+        generateBlocks(xCoord, yCoord, 0f, noBlocksPerColumn, noBlocksPerRow); // foreground
+
+        generateBlocks(xCoord, yCoord, 1f, noBlocksPerColumn, noBlocksPerRow); // background
+    }
+
+    void generateBlocks(float xCoord, float yCoord, float zCoord, int noBlocksPerColumn, int noBlocksPerRow)
+    {
         for (int i = 0; i < noBlocksPerRow; ++i)
         {
             for (int j = 0; j < noBlocksPerColumn; ++j)
             {
-                Instantiate(block, new Vector3((xCoord + j), (yCoord - i), 0f), Quaternion.identity);
+                Instantiate(block, new Vector3((xCoord + j), (yCoord - i), zCoord), Quaternion.identity);
             }
         }
-	}
+    }
 }
